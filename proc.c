ae384b88f184476e1b10270b475b4520439c0159
Phillip Park <ppark006@sledge.cs.ucr.edu>
2018-04-12 18:00:38 -0700

    revised exit definition

--- proc.c
+++ proc.c
@@ -225,11 +225,14 @@ fork(void)
 // An exited process remains in the zombie state
 // until its parent calls wait() to find out it exited.
 void
-exit(void)
+exit(int status)
 {
   struct proc *curproc = myproc();
   struct proc *p;
   int fd;
+
+  //lab1
+  curproc->exit_status = 1;
 
   if(curproc == initproc)
     panic("init exiting");
@@ -266,6 +269,7 @@ exit(void)
   sched();
   panic("zombie exit");
 }
+
 
 // Wait for a child process to exit and return its pid.
 // Return -1 if this process has no children.
